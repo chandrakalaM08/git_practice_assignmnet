bafd02dcd38666443e71013ddac0871cc842338e
sandeshm0re <sandeshmre4@gmail.com>
2022-11-29 22:37:04 +0530

    code fixed

--- prime_problem.js
+++ prime_problem.js
@@ -1,14 +1,14 @@
 let num = 29;
 
-let factor_count = 0;
-for (let i = 1 ; i < N ; i++){
-	if( n % i == 0){
+let count = 0;
+for (let i = 1; i<=num; i++){
+	if( num % i == 0){
 	count++ ;
 	}
 }
 
 if (count == 2){
-	console.log(number," is a prime number.")
+	console.log(num," is a prime number.")
 }
 else{
 	console.log(num,"not prime")
